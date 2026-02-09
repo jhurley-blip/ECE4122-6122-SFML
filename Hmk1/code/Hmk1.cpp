24f9085a8f4ba00b356c18ec1074fd3a8e88a963
jhurley31 <jhurley@gatech.edu>
2026-02-09 10:49:02 -0500

    Updating the main.cpp file to show commandline arguments

--- Hmk1/code/Hmk1.cpp
+++ Hmk1/code/Hmk1.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
-int main()
-{
+using namespace std;
 
+
+int main(int argc, char* argv[]) 
+{
+    cout << "You have entered " << argc
+         << " arguments:" << endl;
+
+    // Using a while loop to 
+    // iterate through arguments
+    int i = 0;
+    while (i < argc) 
+    {
+        cout << "Argument " << i + 1 
+             << ": " << argv[i]
+             << endl;
+        i++;
+    }
+
+    return 0;
 }
